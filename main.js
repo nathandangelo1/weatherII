ed96f6bc6c4fc9027bc4068ba475cd6c36674609
Nate D. D'Angelo <69875151+nathandangelo1@users.noreply.github.com>
2023-05-02 16:34:36 -0500

    added async/await

--- main.js
+++ main.js
@@ -5,117 +5,90 @@ var elementForecastText = document.getElementById("forecast-text");
 document.addEventListener("DOMContentLoaded", function () {
   GetCoords();
 });
-// Step 1: Use geolocation js code (navigator.geolocation.getCurrentPosition() )
-// Step 2: Send latitude and logitude from js code to https://api.weather.gov/points/{latitude},{longitude} using
-//      a get request similar to the one below
-// Step 3: Get the office, gridX, and gridY from step 2
-// Step 4: Plug this info into RequestForecast()
 
 function GetCoords() {
   const status = document.querySelector("#status");
 
-  navigator.geolocation.getCurrentPosition(function (position) {
+  function success(position) {
+    status.textContent = "";
     RequestLocation(position.coords.latitude, position.coords.longitude);
-  });
+  }
 
-  // function success(position) {
-  //   status.textContent = "";
-  //   // const latitude = Math.round(position.coords.latitude * 10000) / 10000;
-  //   // const longitude = Math.round(position.coords.longitude * 10000) / 10000;
-  //   RequestLocation(position.coords.latitude, position.coords.longitude);
-  // }
+  function error() {
+    status.textContent = "Unable to retrieve your location";
+  }
 
-  // function error() {
-  //   status.textContent = "Unable to retrieve your location";
-  // }
-
-  // if (!navigator.geolocation) {
-  //   status.textContent = "Geolocation is not supported by your browser";
-  // } else {
-  //   status.textContent = "Locating…";
-  //   navigator.geolocation.getCurrentPosition(success, error);
-  // }
+  if (!navigator.geolocation) {
+    status.textContent = "Geolocation is not supported by your browser";
+  } else {
+    status.textContent = "Locating…";
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
 }
 
-function RequestLocation(latitude, longitude) {
-  let ajax = new XMLHttpRequest(); //Asynchronous JavaScript And Xml
-  let requestMethod = "GET"; //Give me data
-  let requestUrl = `https://api.weather.gov/points/${latitude},${longitude}`; //Url
-
-  let requestIsAsyncronous = true; //dont hold up wepage when waiting response
-
-  //SEND AJAX REQUEST TO THE URL
-  ajax.open(requestMethod, requestUrl, requestIsAsyncronous); //ajax.open(method, url, async)
-
-  //SET CALLBACK FUNCTION (this function gets called automatically when the response gets back) ***
-  ajax.onreadystatechange = ReturnLocation;
-
-  //SEND REQUEST
-  var data = ajax.send();
+async function RequestLocation(latitude, longitude) {
+  let jdata;
+  try {
+    const response = await fetch(
+      `https://api.weather.gov/points/${latitude},${longitude}`
+    ); //Url
+    jdata = await response.json();
+    console.log(jdata);
+    ReturnLocation(jdata);
+  } catch (error) {
+    console.log("fetch error: ", error);
+  }
 
   console.log("----data----");
-  console.log(data);
+  console.log(jdata);
 } //END FUNCTION
 
-function ReturnLocation() {
-  var responseStatusOk = this.status === 200; //STATUS 200 means OK
-  var responseComplete = this.readyState === 4; //readyState 4 means response is ready
-
-  if (responseStatusOk && responseComplete) {
-    //PARSE THE RESPONSE - convert values to JSON
-    let responseData = JSON.parse(this.responseText);
-
-    //GET THE WEATHER FROM THE RESPONSE TEXT
-    let properties = responseData.properties;
-    let office = properties.gridId;
-    let gridX = properties.gridX;
-    let gridY = properties.gridY;
-    RequestForecast(office, gridX, gridY);
-    // elementForecastText.innerHTML = properties;
-  } else {
-    //SOMETHING WENT WRONG
-    console.log(this);
-  } //end if
+function ReturnLocation(jdata) {
+  //GET THE WEATHER FROM THE RESPONSE TEXT
+  let properties = jdata.properties;
+  let office = properties.gridId;
+  let gridX = properties.gridX;
+  let gridY = properties.gridY;
+  RequestForecast(office, gridX, gridY);
 } //END FUNCTION
 
-function RequestForecast(office, gridX, gridY) {
-  let ajax = new XMLHttpRequest(); //Asynchronous JavaScript And Xml
-  let requestMethod = "GET"; //Give me data
-  let requestUrl = `https://api.weather.gov/gridpoints/${office}/${gridX},${gridY}/forecast`; //Url
-
-  let requestIsAsyncronous = true; //dont hold up wepage when waiting response
-
-  //SEND AJAX REQUEST TO THE URL
-  ajax.open(requestMethod, requestUrl, requestIsAsyncronous); //ajax.open(method, url, async)
-
-  //SET CALLBACK FUNCTION (this function gets called automatically when the response gets back) ***
-  ajax.onreadystatechange = ReturnForecast;
-
-  //SEND REQUEST
-  var data = ajax.send();
-
-  console.log("----data----");
-  console.log(data);
+async function RequestForecast(office, gridX, gridY) {
+  try {
+    const response = await fetch(
+      `https://api.weather.gov/gridpoints/${office}/${gridX},${gridY}/forecast`
+    ); //Url
+    const jdata = await response.json();
+    console.log(jdata);
+    ReturnForecast(jdata);
+  } catch (error) {
+    console.log("fetch error: ", error);
+  }
 } //END FUNCTION
 
-function ReturnForecast() {
-  var responseStatusOk = this.status === 200; //STATUS 200 means OK
-  var responseComplete = this.readyState === 4; //readyState 4 means response is ready
+function ReturnForecast(jdata) {
+  let fl = document.getElementById("flist");
+  //GET THE WEATHER FROM THE RESPONSE TEXT
+  let properties = jdata.properties;
+  let forecast = properties.periods[0];
+  let message = forecast.detailedForecast;
 
-  if (responseStatusOk && responseComplete) {
-    //console.log(this.responseText.properties); //debug
+  elementForecastText.innerHTML = message;
+  let forecastString = "";
 
-    //PARSE THE RESPONSE - convert values to JSON
-    let responseData = JSON.parse(this.responseText);
+  for (var period in properties.periods) {
+    // let daytime = properties.periods[period].daytime; //boolean
+    let name = properties.periods[period].name; //string
+    let short = properties.periods[period].shortForecast; //string
+    let detailed = properties.periods[period].detailedForecast;
 
-    //GET THE WEATHER FROM THE RESPONSE TEXT
-    let properties = responseData.properties;
-    let forecast = properties.periods[0];
-    let message = forecast.detailedForecast;
+    var li = document.createElement("li");
 
-    elementForecastText.innerHTML = message;
-  } else {
-    //SOMETHING WENT WRONG
-    console.log(this); //end if
-  } //END FUNCTION
+    li.classname =
+      properties.periods[period].daytime == true ? "daytime" : "nighttime";
+    li.innerHTML = `<li> ${name} <br> ${detailed} </li>`;
+    let sl = li.appendChild(document.createElement("ol"));
+    sl.className = "forecast";
+    fl.appendChild(li);
+    //document.getElementById("flist").innerHTML = forecastString;
+  }
 }
